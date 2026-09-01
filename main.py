45dab3a58751f0d83a5f8024adc286041906a2b4
hehehahahoobausa <bausa.gb@obmontessori.edu.ph>
2026-09-01 10:17:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ def adding_numbers(e):
     document.getElementById('output').innerHTML = "" # clears previous output
     num1 = float(document.getElementById('neckhurts').value) # get 1st input
     num2 = float(document.getElementById('aurafarm').value)  # get 2nd input
-    result = num1 + num2
-    document.getElementById('outputs').innerHTML = f"The sum is: {result}"
+    result = num1 + num2 # use operator to compute
+    display(result, target = 'output') #display result
